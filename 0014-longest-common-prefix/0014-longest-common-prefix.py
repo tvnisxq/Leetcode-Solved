bdf62b792b8e58defbd9cd89532fd000f4863019
Tanishq sharma <166993496+tvnisxq@users.noreply.github.com>
2025-07-17 17:29:10 +0530

    Update 0014-longest-common-prefix.py
    Uncommented alternative approaches

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,26 +1,28 @@
-# class Solution:
-#     def longestCommonPrefix(self, strs: List[str]) -> str:
-#         # This determines the number of iterations.
-#         min_length = float('inf')
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # This determines the number of iterations.
+        min_length = float('inf')
         
-#         # looping through strings in strs.
-#         for s in strs:
-#             # if length of a string is less than than our min_price;
-#             if len(s) < min_length:
-#                 min_length = len(s)  # update our min_price
+        # looping through strings in strs.
+        for s in strs:
+            # if length of a string is less than than our min_price;
+            if len(s) < min_length:
+                min_length = len(s)  # update our min_price
         
-#         i = 0
-#         # This loop runs until i becomes equal to the min_length.
-#         while i < min_length:
-#             for s in strs:
-#                 # check if the character at ith index of string s is also present in the same index in the first string of strs.
-#                 if s[i] != strs[0][i]:
-#                     # mismatch found: condition = True.
-#                     # returns the common character up until the last iterated i.
-#                     return s[:i]
-#             i += 1
-#         # s[:i] returns empty string if no match is found.
-#         return s[:i] 
+        i = 0
+        # This loop runs until i becomes equal to the min_length.
+        while i < min_length:
+            for s in strs:
+                # check if the character at ith index of string s is also present in the same index in the first string of strs.
+                if s[i] != strs[0][i]:
+                    # mismatch found: condition = True.
+                    # returns the common character up until the last iterated i.
+                    return s[:i]
+            i += 1
+        # s[:i] returns empty string if no match is found.
+        return s[:i] 
+
+
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
