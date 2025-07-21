2153e24b9ccfe150d3086e957fac3866df065d76
Tanishq sharma <166993496+tvnisxq@users.noreply.github.com>
2025-07-21 07:55:25 +0530

    Time: 0 ms (100%), Space: 18.9 MB (33.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,22 @@
-from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Initialize an empty dictionary to store numbers we seen so far with their 
+        # index. {number: index}
         numMap = {}
+
+        # Looping through the list nums. i is the current index and num the current 
+        # number. Enumerate allows us to access both the index and the value at the 
+        # same time. 
         for i, num in enumerate(nums):
-            complement = target - num
+            # Calculates the number we need to find so that num + complement = target.
+            complement = target - num   # if target = 9 and num = 2, then complement = 7.
+
+            # Check if the complement exists in our numMap.
             if complement in numMap:
-                return [numMap[complement], i]
+                return[numMap[complement], i] # numMap[complement] gives the index of the complement, while i is the current index.
+
+            # If no match was found, store the current number and it's index in the numMap for future reference. 
             numMap[num] = i
-        return[]
+        
+        # Safe net in case no pair is found.
+        return [] # But this line is never executed as the problem states that one solution is guaranteed. 
