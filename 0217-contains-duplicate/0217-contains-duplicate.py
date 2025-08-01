7980bcbb0e49dba4189e2edb53f873529500ae1a
Tanishq sharma <166993496+tvnisxq@users.noreply.github.com>
2025-08-01 14:25:03 +0530

    Time: 63 ms (5.17%), Space: 34.9 MB (17.01%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,24 +1,35 @@
-# Brute Force
-# class Solution:
-#     def containsDuplicate(self, nums: List[int]) -> bool:
-#         n = len(nums)
-#         for i in range(n):
-#             for j in range(i+1, n):
-#                 if nums[i] == nums[j]:
-#                     return True
-#         return False
-
-
-
-# Optimal Approach
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # Create an empty hashset to store seen elements
-        seen = set()
-
-        # Iterate through the elements:
+        freq = defaultdict(int)
         for num in nums:
-            if num in seen:
-                return True    # Duplicate Found
-            seen.add(num)
-        return False           # No Duplicates Exist 
+            freq[num] += 1
+
+        for num, count in freq.items():
+            if count > 1:
+                return True  
+        return False
+
+        '''
+            Time:
+                • Best case: O(1)
+                • Worst case: O(n)
+        '''
+
+
+        # Using hashsets
+        # Create an empty hashset to store seen elements.
+        freqSet = set()
+
+        # Iterate through the nums array.
+        for num in nums:
+            if num in freqSet:
+                return True     # Duplicate found
+            freqSet.add(num)    
+        return False            # No Duplicates Exist
+
+        # Is there any more ways to achieve this?
+        # NO this is the most optimal Approach to solve this with:
+        '''
+            Time: O(n) -> Only a single pass is required.
+            Space: O(n) -> In the worst case(all elements are unique) we need to store n elements. 
+        '''
