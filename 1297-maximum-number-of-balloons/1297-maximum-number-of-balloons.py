621e6e7ebeeab8c892af0cca69cd8699f68d890e
Tanishq sharma <166993496+tvnisxq@users.noreply.github.com>
2025-08-02 09:22:22 +0530

    Update 1297-maximum-number-of-balloons.py
    Uncommented Alternate Approaches

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -1,35 +1,35 @@
-# from collections import defaultdict
-# class Solution:
-#     '''
-#         Approach 1: Slightly verbose and Basic.
-#             We use defaultdict to to solve this question because accessing a missing key 
-#             in defaultdict won't raise an IndexError -> As the missing keys are automatically created and assigned a 
-#             default value(0 in case of integers).
-#     '''
-#     def maxNumberOfBalloons(self, text: str) -> int:
+from collections import defaultdict
+class Solution:
+    '''
+        Approach 1: Slightly verbose and Basic.
+            We use defaultdict to to solve this question because accessing a missing key 
+            in defaultdict won't raise an IndexError -> As the missing keys are automatically created and assigned a 
+            default value(0 in case of integers).
+    '''
+    def maxNumberOfBalloons(self, text: str) -> int:
 
-#         counter = defaultdict(int)
-#         balloon = "balloon"
+        counter = defaultdict(int)
+        balloon = "balloon"
 
-#         # For every character in text, check if that character also is in the balloon.
-#         for c in text:
-#             if c in balloon:
-#                 counter[c] += 1
-#                 '''
-#                     If that character is not in the balloon, defaultdict automatically creates a 
-#                     key of that character in the dictionary and immediately increments it by 1.
+        # For every character in text, check if that character also is in the balloon.
+        for c in text:
+            if c in balloon:
+                counter[c] += 1
+                '''
+                    If that character is not in the balloon, defaultdict automatically creates a 
+                    key of that character in the dictionary and immediately increments it by 1.
 
-#                     However, if the character was already present in the dictionary it just performs normal increment on them. 
-#                 '''
-#         # Check whether any character in balloon is missing from the counter.
-#         if any(c not in counter for c in balloon):
-#             return 0  
-#             '''
-#                 No instances of balloon can be created since a character which was required to make a balloon(or more than one      
-#                 balloons) is/are not present. 
-#             '''
-#         else:
-#             return min(counter['b'], counter['a'], counter['l']//2, counter['o']//2, counter['n'])
+                    However, if the character was already present in the dictionary it just performs normal increment on them. 
+                '''
+        # Check whether any character in balloon is missing from the counter.
+        if any(c not in counter for c in balloon):
+            return 0  
+            '''
+                No instances of balloon can be created since a character which was required to make a balloon(or more than one      
+                balloons) is/are not present. 
+            '''
+        else:
+            return min(counter['b'], counter['a'], counter['l']//2, counter['o']//2, counter['n'])
 
 
 from collections import Counter
