3171ebbdf60296aad1abccabb36790f3af662fa7
Tanishq sharma <166993496+tvnisxq@users.noreply.github.com>
2025-08-05 08:07:14 +0530

    Time: 15 ms (49.16%), Space: 22.9 MB (5.1%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,37 +1,37 @@
 from typing import List
 from collections import defaultdict
 class Solution:
-    """ 
-    we use defaultdict: TO avoid having to initialize an empty array for your keys
-    every single time. Provides an empty array or other data structure assigned to a key 
-    by default. 
-    """
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Create a dictionary where each key maps to a list of anagrams.
-        anagramMap = defaultdict(list)
 
-        # Loop over each string in the input list.
+        # Initializes a defaultdict named anagramMap. If a key is accessed that doesn't exist it will automatically 
+        # create a new entry with an empty list ([]) as it's value. 
+        anagrmaMap = defaultdict(list)   
+
+        # Looping through each string s in the input list of strings strs
         for s in strs:
-            # Create a count array for current string
-            # Initialize the count array for 26 lowercase letters all with 0s
-            count = [0] * 26 
+            # And Creating a map of 26 entries and initializes all its elements as 0.
+            # Each entry in this list will store the frequency of the corresponding lowercase english alphabet. 
+            count = [0] * 26
 
-            '''
-            For each character c in String s:
-            # Convert c to its alphabetical index ord('a') = 97.
-            # ord(c) - ord('a') maps 'a' to 0 and 'z' to 25.
-            # Nested loop: over each character of the current string(builds frequency count for current string)
-            '''
+            # Looping throug the individual string s to map the frequencies of the characters in that string
             for c in s:
-                count[ord(c) - ord('a')] +=1
-            # Print statement to check the created hashMap of tuple
-            # print(anagramMap)
+                # According to the ASCII norms ord('a')(a being the very first character of the eng alphabets) is equal to 97 simiarly ord('z') = 122.
+                # So if c = 'a' -> ord('c') - ord('a') = 0 & similarly the last character z maps to 25. Then it increments the count at that index in
+                # in the count list.
+                count[ord(c)-ord('a')] += 1
+            # Print statement to print anagrmaMap dict.
+            # print(anagrmaMap)
 
-            #Convert character count to a hashable key
+            # Type conversion of anagrmaMap dict from list to tuple 
+            # Tuples are hashable and can be used as dictionary keys unlike lists in python.
             key = tuple(count)
 
-            #Group anagrams uisng the key
-            anagramMap[key].append(s)
-        
-        #Return grouped anagrams as list of lists
-        return list(anagramMap.values())
+            # Uses the key(the tuple of character frequencies) to add the current string s 
+            # to the list associated with that key in anagramMap. If the key doesn't exist defaultdict automatically creates 
+            # an empty list for it and then appends the string.
+            anagrmaMap[key].append(s) 
+
+        # Returns a list of all the values from the anagramMap dictionary.
+        # Each value is a list of anagrams(string with same character frequencies), grouped together. 
+        return list(anagrmaMap.values()) 
+
