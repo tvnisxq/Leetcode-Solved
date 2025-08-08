9ea3cbb56459b4fbf77c6ae448c0c25a15a8d6eb
Tanishq sharma <166993496+tvnisxq@users.noreply.github.com>
2025-08-09 03:14:30 +0530

    Time: 0 ms (100%), Space: 18 MB (10.34%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,35 +1,16 @@
-# class Solution:
-#     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        
-        # Approach 1(Brute Force):
-        # Time: O(n.m) -> string lookup is linear
-        # Space: O(1) -> only an integer variable 'count' is used
-
-        # count = 0
-        # for stone in stones:
-        #     if stone in jewels:
-        #         count += 1
-        # return count
-
-
+from collections import defaultdict
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-
         '''
-        Approach 2(Optimal Method):
-        - O(1) constant time lookup to check if something is in the set.
-
-        Time: O(n+m) 
-            - n to build the set  +
-            - m to loop through the elements and doing O(1) lookups: O(m) total
-        Space: O(n)
-            - because of the n jewels which stores upto n characteres if all are unique.
-            
-        '''         
-        setJewels = set(jewels)  #('a', 'A',...)
-
+            Brute Force Approach:
+                If n = len(stones) & m = len(jewels)
+                ▷ Time: O(n.m) -> Iterating through stones and jewels once. String lookup is linear 
+                ▷ Space: O(1) -> Only a variable count is used.
+        '''
         count = 0
+        # Iterating through string literal stones character by character
         for stone in stones:
-            if stone in setJewels:
-                count += 1
+            # if that very character is also present in the string literal jewels. 
+            if stone in jewels:
+                count += 1 # Increment the count varaible by 1.
         return count
