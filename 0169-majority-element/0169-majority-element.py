ac461502e8b25d84d8d9b8ca2b65ef04c575f066
Tanishq sharma <166993496+tvnisxq@users.noreply.github.com>
2025-08-09 00:28:55 +0530

    Time: 8 ms (37.19%), Space: 19.2 MB (86.71%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,54 +1,20 @@
-# Brute Force: using Nested Loops
-# class Solution:
-#     def majorityElement(self, nums: List[int]) -> int:
-#         for i in nums:
-#             if nums.count(i) > len(nums)/2:
-#                 return i
-
-'''
-    Time: O(n²): counting frequency using nested loops.
-    Space: O(1): just a few temporary variables are used.
-'''
-# Optimal solution: Using Boyer Moore Voting Algorithm
-'''
-When to use:
-    • If an element occurs more than n/2 times, it will always be the last one standing after
-    pairing off different elements.
-
-    • Used When:
-        • Majority element is required(Or in cased where it
-        is given majority element is occuring > n/2 times) 
-
-        • Dominant element is required.    
-
-'''
+from collections import defaultdict
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # Used to track the occurences of elements. 
-        count = 0
+        n = len(nums)
+        
+        # Using defaultdict to avoid missing character key frequencies.
+        freq_count = defaultdict(int)
 
-        # Placeholder for the potential majority element.
-        # will be updated throughout the loop based on the current count.
-        candidate = None
-
-        # Iterate therough entire list to check
-        # what the majority element(candidate) is.
+        # Iterating through the nums array to add num frequencies
         for num in nums:
-            # if count drops to 0: -> no candidate
-            # therefore we assign a new one.
-            if count == 0:
-                candidate = num
-            # if current number == candidate -> count += 1 else: count -= 1.    
-            count += (1 if num == candidate else -1)
-        # only element standing with count > 0 will be the majority element. 
-        return candidate
+            freq_count[num] += 1
+            # print(freq_count) # To check whether freq_count is created successfully.
 
-# Eventually the real majority element will survive as it is guaranteed to be 
-# occuring more than n/times, so it will always outweigh others.
+        # Iteratimg through the freq_count to check for count.
+        for num, freq in freq_count.items():
+            if freq_count[num] > n/2:
+                return num
+            
 
-'''
-    Time: O(n): Only using one pass through the array.
-
-    Space: O(1): we only use two vatiables(count & candidate)
-                 regardless of the size of the input array.
-'''
+                    
