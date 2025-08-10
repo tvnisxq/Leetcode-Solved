de54e01f0f602b9be0d225a7a05d0b8747bed035
Tanishq sharma <166993496+tvnisxq@users.noreply.github.com>
2025-08-11 02:38:33 +0530

    Time: 7 ms (3.94%), Space: 17.7 MB (80.67%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,67 +1,27 @@
-# class Solution:
-#     def isValid(self, s:str) -> bool:
-
-#         '''
-#             Brute Force Solution:
-#             • Time: O(n²) 
-#                 - Each iteration scans entire string -> O(n).
-#                 - Maximum n/2 iterations needed -> O(n)
-#                 Total: O(n²)
-
-#             • Space: O(n) for string operations.
-#         '''
-#         # keep removing valid characters until there are no changes.
-#         while True:
-#             original_length = len(s)
-
-#             # Remove valid pairs.
-#             s = s.replace('()', '')
-#             s = s.replace('{}', '')
-#             s = s.replace('[]', '')
-
-#             # If no change is made: It's a valid parentheses.
-#             if len(s) == original_length:
-#                 break
-
-#         return len(s) == 0
-
-
-
-
 class Solution:
     def isValid(self, s: str) -> bool:
         '''
-            Implememting Stack(LIFO order):
-           • For multi type parentheses check we need to use stack to solve this.
-           • For each closing bracket character in string s, we check if it's corresponing 
-            opening character exists in our map.
-           • This is the most optimal solution.
-
-
-           Time: O(n)  
-            - Single pass through string, each character is processed exactly once.
-            - Stack operations push/pop are O(1).
-
-           Space: O(n)
-           - Stack can hold up to n/2 opening brackets in worst case.
+            Brute Force:
+                ▷ Time: 
+                    • each iteration scans entire string - O(n)
+                    • maximum n/2 iterations needed - O(n)
+                        TOTAL: O(n²)
+                ▷ Space:
+                    replace() method creates a new string every time it's called bcoz strings in python cannot be changed in place.
+                    The largest string you ever hold in memory during this algorithm is the original one (size n).
+                    Since .replace() allocates a new string while the old one is still in memory, peak memory usage is: O(n) + O(n)
+                        TOTAL: O(2n) -> O(n)
         '''
-        # Initialize empty List which acts our stack:
-        stack = []
-        mapping = {
-            ')': '(', ']':'[', '}': '{'  # key:value -> closing: opening
-        }
+        # keep removing valid characters until there are none left
+        while True: 
+            true_length = len(s) # len function of string has a constant time complexity: O(1)
 
-        for char in s:
-            # check if this character is a closing brakcet.
-            if char in mapping:
-                # If stack is empty -> there's no opening bracket to match.
-                # stack.pop() -> remove & return the most recent opening bracket
-                # mapping[char] -> fetches the opening bracket(value) that should match this closing bracket(key).
-                # If the popped character is not equal to the fetched opening bracket: return False.
-                if not stack or stack.pop() != mapping[char]:
-                    return False
-            # If char is not an opening bracket it must be a closing bracket.
-            else:
-                stack.append(char)
-        # since return type is bool: if the len(stack) is equal to 0 -> valid & False otherwise.
-        return len(stack) == 0
+            s = s.replace('()','')    
+            s = s.replace('{}','')    
+            s = s.replace('[]','')
+
+            # If no change is made it's a valid parenthesis.
+            if len(s) == true_length:            
+                break
+
+        return len(s) == 0 
