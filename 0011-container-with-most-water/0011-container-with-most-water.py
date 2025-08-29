f48ab9440e4bd1298f5d49b1eecbccfc6afc20a8
Tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-08-29 12:52:28 +0530

    Time: 115 ms (11.19%), Space: 28.4 MB (67.86%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -0,0 +1,42 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        '''
+            Two Pointers(Optimal Solution):
+
+            • TIME: O(n) -> loop runs atmost n times where n is the number of
+            elements in the height list. 
+            • Space: O(1) -> The algorithm uses a constant number of variables, 
+            no additional data structures are used.
+        '''
+        # initializing the pointers
+        l, r = 0, len(height) - 1 
+        # creating a variable to store max_area
+        max_area = 0
+
+        # loop traverses until left & right pointers merge at a common point,
+        # and hence area can't be calculated using a single height
+        while l < r:
+
+            width = r - l 
+            curr_height = min(height[l], height[r]) # taking the minimum of the heights at left & right
+            area = width * curr_height # calculating area using (Length*Breadth)
+            max_area = max(max_area, area) # opting max_area to whatever(max_are or area) the maximum comes to be.
+
+            # if height of left is less than height of right,
+            # so we need to increase the height of l to move towards maximum area
+            if height[l] < height[r]:
+                l += 1 # so we increment the left pointer
+            else:
+                r -= 1
+
+            # if either height of left is more than height of 
+            # right -> we need to increase the height of r to move towards maximum area 
+            '''
+            OR
+            '''
+            # if the height of the left & right become equal, it actually doesn't
+            # matter whichever pointer is shifted. because at one point or another 
+            # the pointers will eventually meet and the loop will terminate.
+
+        # After the loop finishes return the maximum area found.
+        return max_area
