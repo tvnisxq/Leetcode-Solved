c84a36896e7ba9365dbbebff1366892591c0dd34
Tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-08-29 12:58:27 +0530

    Time: 107 ms (38.51%), Space: 28.4 MB (67.86%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,19 +1,19 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        
+
         # initializing the pointers
         l, r = 0, len(height) - 1 
         # creating a variable to store max_area
-        max_area = 0
+        max_a = 0
 
         # loop traverses until left & right pointers merge at a common point,
         # and hence area can't be calculated using a single height
         while l < r:
 
-            width = r - l 
-            curr_height = min(height[l], height[r]) # taking the minimum of the heights at left & right
-            area = width * curr_height # calculating area using (Length*Breadth)
-            max_area = max(max_area, area) # opting max_area to whatever(max_are or area) the maximum comes to be.
+            w = r - l 
+            h = min(height[l], height[r]) # taking the minimum of the heights at left & right
+            a = w * h # calculating area using (Length*Breadth)
+            max_a = max(max_a, a) # opting max_area to whatever(max_are or area) the maximum comes to be.
 
             # if height of left is less than height of right,
             # so we need to increase the height of l to move towards maximum area
@@ -22,4 +22,4 @@ class Solution:
             else:
                 r -= 1
        # After the loop finishes return the maximum area found.
-        return max_area
+        return max_a
