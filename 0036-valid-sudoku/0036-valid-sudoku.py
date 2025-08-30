a33a45ef334d6ddc3f5998df1c412bba663ec78b
Tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-08-30 09:24:55 +0530

    Time: 11 ms (9.87%), Space: 17.5 MB (99.07%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,33 +1,38 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         '''
-            Brute Force Approach:
-            •
-            •
-            •
+            Brute Force Solution:
         '''
-        # Iterate through each cell in the 9x9 sudoku board.
-        for i in range(9):  # For each row 
-            for j in range(9):  # For each column
-                if board[i][j] == '.':   # Checking for filled cells only
-                    continue
-
-                # Logic for Checking Columns:
+        # Iterate over each row & column of the 9x9 board:
+        for i in range(9): # for each row 
+            for j in range(9): # for each column
+                if board[i][j] == '.': # if the cell is empty
+                    # continue because we need to validate only the filled cells
+                    continue 
+                          
+                # Logic for checking Rows:
+                for row in range(9):
+                    # We want to check in every other cell if the
+                    # val at current cell is equal to val at any of the 
+                    # other cells
+                    if row != i and board[row][j] == board[i][j]: 
+                        return False
+            
+                #  Logic for checking Cols:
                 for col in range(9):
                     if col != j and board[i][col] == board[i][j]:
                         return False
-                
-                # Logic for Checking Rows:
-                for row in range(9):
-                    if row != i and board[row][j] == board[i][j]:
-                        return False
-                
-                # Logic for Checking the 3x3 Boxes:
-                start_row = 3*(i//3)
-                start_col = 3*(j//3)
 
-                for x in range(start_row, start_row+3):
-                    for y in range(start_col, start_col+3):
-                        if(x!=i or y!=j) and board[x][y] == board[i][j]:
+                # Logic for checking the 3x3 boxes:
+                start_row = 3 * (i//3)
+                start_col = 3 * (j//3)
+
+                # We want to consider every other cell except the cell that we're in
+                # if we want to check the other cell whether they're having the same val
+                # as our current cell 
+                for x in range(start_row, start_row + 3):
+                    for y in range(start_col, start_col + 3):
+                        if (x != i or y != j) and board[x][y] == board[i][j]:
                             return False
         return True
+                
