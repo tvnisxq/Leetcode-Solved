792baf0675ecc181a8690195349426b5b5b1369a
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-09-04 11:22:55 +0530

    Time: 0 ms (100%), Space: 17.7 MB (82.02%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.py
+++ 3830-find-closest-person/3830-find-closest-person.py
@@ -3,9 +3,7 @@ class Solution:
         abs_x = abs(x-z)
         abs_y = abs(y-z)
 
-        if abs_x < abs_y:
-            return 1
-        elif abs_x > abs_y:
-            return 2
-        else:
-            return 0
+        if abs_x < abs_y: return 1
+        elif abs_x > abs_y: return 2
+        else: return 0
+            
