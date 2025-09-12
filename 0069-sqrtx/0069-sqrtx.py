ea8c76fe1516ffc3547284c52d577680db051960
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-09-13 01:50:04 +0530

    Time: 3 ms (59.4%), Space: 17.9 MB (32.81%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,26 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        """ Binary Search"""
+        # Time: O(log n)
+        # Space: O(1)
+        # Edge case: If x is 1 or 0
+        if x < 2:
+            return x
+        
+        left, right = 1, x
+
+        while left <= right:
+            middle = (left + right) // 2
+            sqr = middle * middle
+
+            if sqr == x:
+                return middle
+
+            elif sqr > x:
+                right = middle - 1
+            
+            else:
+                left = middle + 1
+        
+        # When loop finishes, right is the integer part of sqrt(x)
+        return right
