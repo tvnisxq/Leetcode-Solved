e7e65898085950de62eaca2c12202f204d4592b5
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-09-13 02:30:49 +0530

    Time: 0 ms (100%), Space: 18.6 MB (79.6%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,24 +3,19 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        '''
-            Iterative Solution(Using Two Pointers):
-                ▷ Time: O(n)
-                ▷ Space: O(1)
-        '''
-        # Initialising the pointers.
-        prev, curr = None, head
+        """ Iterative Solution(Two Pointers)"""
+        # Time: O(n)
+        # Space: O(1)
 
-        # Looping through the Linked List.
+        # initializing the pointers
+        prev, curr = None, head     # prev is None & curr points to head
+
+        # Looping through the list
         while curr:
-            nxt = curr.next # temporary variable to keep track of original links.
-            curr.next = prev # pointing the nodes to prev instead of next(reversal).
-            prev = curr # shifting the prev pointer to the curr position.
-            curr = nxt # moving the traversal pointer one step forward in original linked list.
-
-        # prev is returned as the head of the newly reversed list. 
-        return prev 
+            nxt = curr.next # For keeping the original links
+            curr.next = prev # Pointing the nodes to prev instead of next(reversal)
+            prev = curr # Moving the current pointer to current position
+            curr = nxt # Moving the traversal pointer one step forward in original LL.
+        return prev # Prev is returned as the head of the newly reversed list.
