cfa7341214b6f073b91ec593500d3c238f573168
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-09-13 18:35:41 +0530

    Time: 57 ms (13.96%), Space: 39.4 MB (38.68%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,33 +5,21 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        '''
-            Converting the linked list to array:
-            ▷ Time: O(n) -> 
-            ▷ Space: O(n) -> 
-        '''
-        # Create an empty list to store LL nodes
+        """ Two pointers approach"""
+        # TIme: O(n)
+        # Space: O(n)
         nums = []
 
         while head:
-            nums.append(head.val) # Append single node per iteration 
-            head= head.next # Move the head pointer to the next node.
+            nums.append(head.val)
+            head = head.next
 
-        # Initializing two pointers: left(l) starting from beginning and right(r) starting from end.
-        l, r = 0, len(nums) - 1
-        # Loop runs till the left pointer's index is less than or equal to right pointer's index.
-
+            l, r = 0, len(nums)-1
+        
         while l <= r:
-            # If the value stored in list at the index of left pointer 
-            # is not equal to that of left pointer -> CAN'T BE A PALINDROME. 
-
-            if nums[l] != nums[r]: 
-                return False # Hence return False
-
-            # Else increment the pointer's position    
+            if nums[l] != nums[r]:
+                return False
             l += 1
             r -= 1
-
-        # Return True if the entire loop has finished executing and no anomalies we're found
-        return True
         
+        return True
