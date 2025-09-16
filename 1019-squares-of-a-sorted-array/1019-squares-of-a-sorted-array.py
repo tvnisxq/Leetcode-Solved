8538d503a74a79bc8c6fadd8c54ff0985dfc064e
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-09-16 21:10:27 +0530

    Time: 9 ms (60.34%), Space: 19.6 MB (50.53%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
@@ -1,43 +1,17 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        '''
-            Two Pointers Algorithm:
-            ▷ Left pointer is placed at the beginning & the right at the end of the array
-            ▷ The left pointer moves to the right, the right pointer moves to the left,
-            squeezing the array, until they cross each other.
-        '''
-        # left pointer at the beginning
-        left = 0
-        # right pointer at the end
-        right = len(nums) - 1
-        # empty array to store the result
-        result = []
+        n = len(nums)
+        l, r = 0, n-1
+        res = [0] * n
 
-        # The loop runs while the left & right pointer are still
-        # not equal or left one is at an index than that of the right pointer.
-        while left <= right:
-            '''
-            Here we take absolute values of the array because it 
-            addresses the effect of squaring negative numbers.
-            '''
-            # if absolute of nums at left is greater than the absolute of nums at right:
-            if abs(nums[left]) > abs(nums[right]):
-                # square the number and append it to the result
-                result.append(nums[left] ** 2)
-                # move the left pointer one step ahead
-                left += 1
-            
-            # however if the absolute of nums at right is greater or equal
-            # than the absolute of nums at left:
+        for i in range(n-1, -1, -1):
+            if abs(nums[l]) > abs(nums[r]):
+                val = nums[l]
+                l += 1
             else:
-                # square the number and append it.
-                result.append(nums[right] ** 2)
-                # move the right pointer ahead by 1 step
-                right -= 1
-
-        # Currently the list is sorted in a decreasing order
-        # so we reverse it.
-        result.reverse()
+                val = nums[r]
+                r -= 1
+            
+            res[i] = val ** 2
         
-        # then finally return it.
-        return result
+        return res
