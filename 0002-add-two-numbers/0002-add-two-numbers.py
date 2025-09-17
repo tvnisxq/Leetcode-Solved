b754b90fa2ede9c7a030311a8b1e0320684e5aba
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-09-18 00:19:54 +0530

    Time: 6 ms (36.37%), Space: 17.8 MB (84.73%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -13,15 +13,13 @@ class Solution:
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
         
-            # Do the calculations
+        
             total = val1 + val2 + carry
             carry = total // 10
 
-            # Set the value and move current to next position
             current.next = ListNode(total % 10)
             current = current.next
 
-            # Move l1 and l2 to next
             if l1:
                 l1 = l1.next
             
