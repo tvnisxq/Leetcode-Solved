a3ae6f500a98d98240aa7049f790bd853e73be1b
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-09-21 00:44:12 +0530

    Time: 0 ms (100%), Space: 22.9 MB (83.18%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,21 +3,4 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        '''
-            Two pointer approach:
-        '''
-        # taking the length of the string.
-        n = len(s)
-        '''
-        Initializing the two pointers:
-        one in the start index & the other in the last
-        '''
-        l = 0
-        r = n - 1
-
-        # Loop runs until the left pointer & right pointer becomes equal:
-        while l < r:
-            s[l], s[r] = s[r], s[l]
-            # incrementing the left & right pointers
-            l += 1
-            r -= 1
+        s.reverse()
