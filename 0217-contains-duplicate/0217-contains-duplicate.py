bf58b95160df79c54946d1d4f48168b7d812378c
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-09-27 22:21:28 +0530

    Time: 59 ms (5.15%), Space: 34.9 MB (9.91%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,15 +1,12 @@
 from collections import defaultdict
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        '''
-            Optimal Approach:
-                ▷ Time: O(n) -> n is the number of elements in the input list.
-                ▷ Space: O(k) -> k is the number of unique elements.
-        '''
         freq = defaultdict(int)
+
         for num in nums:
             freq[num] += 1
+        
         for num, count in freq.items():
             if count > 1:
-                return True
+                return True 
         return False
