c91c780383e0e2e65cf1673c36dccf8490f0f1bd
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-09-27 23:47:38 +0530

    Time: 10 ms (74.98%), Space: 17.7 MB (86.6%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        
+        # Base Case
         if len(s) != len(t):
             return False
         
-        return sorted(t) == sorted(s)
+        map = {}
+        
+        for c in s:
+            map[c] = map.get(c, 0) + 1
+
+        for c in t:
+            if c not in map:
+                return False
+            map[c] -= 1
+
+        for val in map.values():
+            if val != 0:
+                return False
+        
+        return True
+
