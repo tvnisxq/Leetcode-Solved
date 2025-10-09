16c235c5716cc2b3d7a441af44cc5e6ca2939d45
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-10-09 10:11:13 +0530

    Time: 0 ms (100%), Space: 19.1 MB (23.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Edge Case
         if not nums or len(nums) < 2:
-            return False
+            return []
         
         numMap = {}
         for i, num in enumerate(nums):
@@ -10,3 +11,5 @@ class Solution:
             if complement in numMap:
                 return [numMap[complement], i]
             numMap[num] = i
+        
+        return []
