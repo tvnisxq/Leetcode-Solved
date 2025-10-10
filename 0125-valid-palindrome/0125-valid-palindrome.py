88a5a6a3dfe17197681f83bd432299f995fc6c50
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-10-11 01:48:56 +0530

    Time: 12 ms (17.82%), Space: 17.8 MB (95.22%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,40 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        '''
-            Two methods:
-            ▷ 1. Two pointers: Cheat method.
-            ▷ 2. Two pointers: Cheat method short.
-            ▷ 3. Two pointers: function creation to handle alphanumeric characters.
-        '''
-        # ▷ CHEAT METHOD
-        # Initializing two pointers:
         n = len(s)
-        l = 0
-        r = n - 1 
+        l, r = 0, n - 1
 
-        # Loop runs until left and right pointers converge at a common point.
         while l < r:
-            # This is where we cheat by using the builtin .aplhanum python function.
-            # If the character at l is not an alphanumeric:
             if not s[l].isalnum():
-                l += 1 # move the left pointer one step ahead
-                continue 
-            # If the character at r is not an aplhanumeric:
-            if not s[r].isalnum():
-                r -= 1 # move the right pointer one step ahead(from the end)
+                l += 1
                 continue
             
-            # If the lowercase of character at l is not same as lowercase 
-            # of the character at r -> they're not a palindrome 
+            if not s[r].isalnum():
+                r -= 1
+                continue
+            
             if s[l].lower() != s[r].lower():
-                return False # hence return False
-
-            '''
-            Moving the pointers inward either way cuz the loop will terminate 
-            only when they converge at a common point. This is for moving past a valid,
-            matching pair of characters
-            '''
+                return False
+            
             l += 1
             r -= 1
-
+        
         return True
