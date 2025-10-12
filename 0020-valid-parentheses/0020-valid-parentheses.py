185df37400237d15f6e55ac9ba73df32d9118cc1
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-10-12 09:18:22 +0530

    Time: 3 ms (32.54%), Space: 17.7 MB (85.2%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,16 +3,15 @@ class Solution:
         stack = []
         mapping = {
             ')': '(',
-            '}': '{',
-            ']': '['
+            ']': '[',
+            '}': '{'
         }
 
         for char in s:
             if char in mapping:
                 if not stack or stack.pop() != mapping[char]:
                     return False
-            
             else:
                 stack.append(char)
-        
+
         return len(stack) == 0
