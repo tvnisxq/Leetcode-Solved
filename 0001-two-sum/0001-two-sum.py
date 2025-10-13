6b09597f1f41e4eebe5bc1e495e079896bcb8394
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-10-14 01:13:52 +0530

    Time: 4 ms (41.39%), Space: 19.2 MB (10.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Edge Case
-        if not nums or len(nums) < 2:
-            return []
-        
+        """Optimal Approach"""
         numMap = {}
         for i, num in enumerate(nums):
             complement = target - num
-
             if complement in numMap:
                 return [numMap[complement], i]
             numMap[num] = i
-        
-        return []
+
+
+
+
