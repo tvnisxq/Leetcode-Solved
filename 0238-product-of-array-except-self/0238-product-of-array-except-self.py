1fa92905d4e9024dc831522501880a48b0e5c462
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-10-15 23:18:49 +0530

    Time: 19 ms (86.74%), Space: 23.3 MB (80.29%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,21 +3,15 @@ class Solution:
         n = len(nums)
         answer = [1] * n
 
-        # First calculate all the prefix products
-        # Product of all the elements to the left of a given element
-        # Assume left index = 1.
-        prefix = 1
+        prefix = 1 
         for i in range(n):
             answer[i] = prefix
             prefix *= nums[i]
         
-        # Second Pass: Calculate suffix products and multiply
-        # Product of all the elements to the right of the array
-        # At last element assume last element = 1
-        # Multiply prefix and suffix products
         suffix = 1
         for i in reversed(range(n)):
             answer[i] *= suffix
             suffix *= nums[i]
-
+        
         return answer
+        
