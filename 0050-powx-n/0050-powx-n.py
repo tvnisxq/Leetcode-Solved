39f0ec54a611c819e4c091bd1f3faf8ae62913cc
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-10-17 14:52:40 +0530

    Time: 0 ms (100%), Space: 18.1 MB (11.24%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,17 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        
         def helper(x, n):
-        # Handling the edge cases
-            if x == 0: return 0 
-            if n == 0: return 1 
-            
-            # Core Logic for recursion
+            if x == 0: return 0
+            if n == 0: return 1
+
             res = helper(x, n // 2)
             res = res * res
-            
-            # If n is odd multiply by x once more
-            return x * res if n % 2 else res  
 
+            return x * res if n % 2 else res
+        
         res = helper(x, abs(n))
-        return res if n >= 0 else 1 / res
+        return res if n >= 0 else 1/res
