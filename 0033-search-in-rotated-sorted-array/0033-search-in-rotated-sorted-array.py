cedb1f119b2f1432d7d872cdcea980410fce431c
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-10-24 00:02:32 +0530

    Time: 0 ms (100%), Space: 18.3 MB (22.07%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,25 +1,29 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # First BS(traditional) to find the minimum element
         n = len(nums)
-        left = 0
-        right = n - 1
+        left, right = 0, n - 1
 
         while left < right:
             middle = (left + right) // 2
+
             if nums[middle] > nums[right]:
                 left = middle + 1
             else:
                 right = middle
         
-        min_index = left
+        min_index = right
 
+        # Setting up the range for another BS which is going to check
+        # if the minimum element is the target element
         if min_index == 0:
             left, right = 0, n - 1
         elif nums[0] <= target <= nums[min_index - 1]:
             left, right = 0, min_index - 1
         else:
             left, right = min_index, n - 1
-
+        
+        # Second BS
         while left <= right:
             middle = (left + right) // 2
 
@@ -29,5 +33,5 @@ class Solution:
                 left = middle + 1
             else:
                 right = middle - 1
-        
+                
         return -1
