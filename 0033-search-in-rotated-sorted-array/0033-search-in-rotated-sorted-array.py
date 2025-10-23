9e1ec4ec6cb662193972e7b44cb3874c6f6380da
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-10-24 00:24:01 +0530

    Time: 0 ms (100%), Space: 18.1 MB (66.57%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,6 +1,6 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        # First BS(traditional) to find the minimum element
+    def search(self, nums: List[int], target: int) -> int: 
+        """First BS(traditional) for calculating the min element"""
         n = len(nums)
         left, right = 0, n - 1
 
@@ -14,24 +14,30 @@ class Solution:
         
         min_index = right
 
-        # Setting up the range for another BS which is going to check
-        # if the minimum element is the target element
+        """Setting up the bounds for another BS"""
+        # Edge case where we run a traditional BS again
         if min_index == 0:
-            left, right = 0, n - 1
+            left, right = 0, n - 1 
+        
+        # If the target is in the left partition
         elif nums[0] <= target <= nums[min_index - 1]:
             left, right = 0, min_index - 1
+        
+        # The target then must be in the right partition
         else:
             left, right = min_index, n - 1
         
-        # Second BS
+        """Second BS"""
         while left <= right:
             middle = (left + right) // 2
 
             if nums[middle] == target:
                 return middle
+
             elif nums[middle] < target:
                 left = middle + 1
+
             else:
                 right = middle - 1
-                
+        
         return -1
