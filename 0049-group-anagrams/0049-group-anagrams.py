d9ffc4c68d428a1640deece130f3a7e9029f194d
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-10-23 12:01:37 +0530

    Time: 11 ms (83.37%), Space: 21.2 MB (47.52%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,29 @@
 from collections import defaultdict
-from typing import List
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        we use defaultdict to avoid having to initialize an empty array
+        for your keys every single time. Provides an empty array or any 
+        other data structure assigned to a key by default.
+        """
         
+        # Initialize an empty hashMap using List
         anagramMap = defaultdict(list)
-        for s in strs:
-            count = [0] * 26
+        # Result will be stored in an empty array
+        result = []
 
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            key = tuple(count)
-            anagramMap[key].append(s)
-        
-        return list(anagramMap.values())
+        for s in strs:
+            # Sorted method returns back a list which is mutable data type 
+            # and mutable data types can't be keys
+            sorted_s = tuple(sorted(s)) # Sorts string in alphabetically increasing order and converting type to tuple 
+            anagramMap[sorted_s].append(s) # adding a sorted version of that as a key in the map
+            # print(anagramMap)
+
+        """ 
+        For adding all the values in the map
+        and appending them in the result.
+        Iterating over all the values in the map -> returns the list of values
+        """ 
+        for value in anagramMap.values():
+            result.append(value)
+        return result
