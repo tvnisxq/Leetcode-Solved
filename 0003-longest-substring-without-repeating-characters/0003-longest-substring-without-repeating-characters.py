336bde65bb9b7e988412e2a61f9ef858bbc2982f
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-10-29 17:36:46 +0530

    Time: 14 ms (83.52%), Space: 17.7 MB (96.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,15 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # Time: O(n); 
-        # Space: O(m); m is the number of umique characters stored in set
-        charSet = set()
+        charset = set()
         l = 0
         res = 0
 
         for r in range(len(s)):
-            while s[r] in charSet:
-                charSet.remove(s[l])
+            while s[r] in charset:
+                charset.remove(s[l])
                 l += 1
-            charSet.add(s[r])
-            res = max(res, r - l+ 1)
+            charset.add(s[r])
+            res = max(res, r- l + 1)
         return res
+        
