1bc819016f5a7ef4ea2597f60b9c8669e5cd0816
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-11-01 15:38:40 +0530

    Time: 2 ms (35.27%), Space: 17.9 MB (92.43%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,27 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        """
+        1. Only add open parenthesis if open < n
+        2. Only add a closing parenthesis if closed < open
+        3. Valid iff open == closed == n
+        """
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+            
+        backtrack(0, 0)
+        return res
