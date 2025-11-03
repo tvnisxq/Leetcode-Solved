d00f0b247e41f7c85fb16b764bfda93b0e9c4ef5
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-11-04 02:14:49 +0530

    Time: 0 ms (100%), Space: 18 MB (11.19%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,6 +1,6 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        def helper(x, n):
+        def helper(X, n):
             if x == 0: return 0
             if n == 0: return 1
 
