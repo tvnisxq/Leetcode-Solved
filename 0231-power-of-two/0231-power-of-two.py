43e3391c31aee8f30b923c3198d13121a714bb67
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-11-09 00:03:28 +0530

    Time: 0 ms (100%), Space: 17.9 MB (14.28%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,14 +1,12 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        """
-        Basic Implementation:
-        """
-        power = 1
-        count = 1
 
+        power = count = 1
+        
         while power <= n:
             if power == n:
                 return True
             power = 2 ** count
             count += 1
+        
         return False
