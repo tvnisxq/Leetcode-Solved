fcf359f7586f208e136e2b53d7d372a0c4a1e23b
tanishq <166993496+tvnisxq@users.noreply.github.com>
2025-11-08 11:07:36 +0530

    Time: 22 ms (48.45%), Space: 17.7 MB (99.62%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,14 +1,17 @@
-from collections import defaultdict
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         dictMag = defaultdict(int)
-
+        
+        # Loop over the characters of magazine
         for m in magazine:
-                dictMag[m] += 1
+            dictMag[m] += 1 # increment the count of characters as we see them 
         
+        # Loop over the chars of ransomNote
         for r in ransomNote:
-            if dictMag[r] <= 0:
+            if dictMag[r] <= 0: # if char count is negative or zero we can't use it
                 return False
-            dictMag[r] -= 1
+            dictMag[r] -= 1 # otherwise just decrement the char count for next iteration
         
+        # if all chars of ransomNote are completed using 
+        # chars of magazine, 
         return True
