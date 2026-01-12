6a157d51f9f18dddd223c461ce29700bba7483ad
tanishq <166993496+tvnisxq@users.noreply.github.com>
2026-01-13 00:37:56 +0530

    Time: 0 ms (100%), Space: 19.5 MB (6.59%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,16 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        x, y = m-1, n-1
-        
-        for z in range(m + n - 1, -1, -1):
+        x ,y = m-1, n-1
+
+        for z in range(m+n-1, -1, -1):
             if x < 0:
                 nums1[z] = nums2[y]
                 y -= 1
             
             elif y < 0:
                 break
-            
+
             elif nums1[x] > nums2[y]:
                 nums1[z] = nums1[x]
                 x -= 1
@@ -20,5 +20,5 @@ class Solution:
             else:
                 nums1[z] = nums2[y]
                 y -= 1
-            
-                
+        
+        return nums1
