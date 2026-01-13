e08086398a9ae93f6ac1aeae7dd07aab1e4339b7
tanishq <166993496+tvnisxq@users.noreply.github.com>
2026-01-14 01:13:06 +0530

    Time: 0 ms (100%), Space: 20 MB (10.85%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -5,14 +5,13 @@ class Solution:
 
         while l <= r:
             m = (l+r) // 2
-
             if nums[m] < target:
                 l = m + 1
             elif nums[m] > target:
                 r = m - 1
             else:
                 return m
-        
+            
         if nums[m] < target:
             return m + 1
         else:
