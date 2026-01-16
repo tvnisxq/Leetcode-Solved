d96385791f7884c12c2ace1004fb0c942c4fe71a
tanishq <166993496+tvnisxq@users.noreply.github.com>
2026-01-16 19:41:05 +0530

    Time: 53 ms (36.15%), Space: 19.1 MB (16.88%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,6 +1,22 @@
+# MEMOIZATION Approach
+# Time: O(n) -> Each num is calculated once
+# Space: O(n) -> for the cache and the recursion stack
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0: return 0
-        elif n == 1: return 1
-        else:
-            return self.fib(n-1) + self.fib(n-2)
+        # Create a cache to store computed results
+        memo ={}
+
+        def helper(num):
+            # Base Cases
+            if num == 0: return 0
+            if num == 1: return 1
+
+            # Check if we've already calculated this
+            if num in memo:
+                return memo[num]
+
+            # Calcuate and store in cache
+            memo[num] = helper(num-1) + helper(num-2)
+            return memo[num]
+
+        return helper(n)
