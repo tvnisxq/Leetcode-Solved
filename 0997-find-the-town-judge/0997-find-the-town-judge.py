ae0050986d49ccfc05d2f4e82d2ff877ff365a29
tanishq <166993496+tvnisxq@users.noreply.github.com>
2026-01-19 01:31:56 +0530

    Time: 23 ms (27.88%), Space: 22.5 MB (19.25%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -5,10 +5,16 @@ class Solution:
         outgoing = defaultdict(int)
 
         for src, dst in trust:
-            incoming[dst] += 1
             outgoing[src] += 1
+            incoming[dst] += 1
 
+        # Incoming must be n-1 and outgoing must be 0
         for i in range(1, n+1):
+            # Meaning the town judge trusts nobody(including himself: 0)
+            # and everybody in the town trusts him except himself(n-1).
             if outgoing[i] == 0 and incoming[i] == n-1:
                 return i
         return -1
+
+# Time: O(v+e)
+# Space: O(v)
